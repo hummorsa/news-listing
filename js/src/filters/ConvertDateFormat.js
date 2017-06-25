0e1ad335f84900ea39fa99b80dee3f94e7720f7e
HumbertoM <hummorsa@Humbertos-MacBook-Pro.local>
2017-06-25 11:55:08 -0600

    validation

--- js/src/filters/ConvertDateFormat.js
+++ js/src/filters/ConvertDateFormat.js
@@ -5,6 +5,8 @@
 
 dotCMSApp.filter('ConvertDateFormat',['$filter',function($filter){
   return function(){
-      return $filter('date')(new Date(arguments[0].split(' ')[0]), arguments[1])
+      if (arguments[0] != undefined){
+          return $filter('date')(new Date(arguments[0].split(' ')[0]), arguments[1])
+      };
   }
 }]);
